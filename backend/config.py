4b7820f8cc810a59b617408ce6d9847b95123e2c
WalterMonteirodabliu <walter.felipemonteiro@gmail.com>
2025-11-04 13:06:26 -0300

    fix: remove config.py

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "host": "shuttle.proxy.rlwy.net",  # IP do servidor de banco de dados
-    "port": 11674,
-    "user": "root",        # ou outro usuário MySQL
-    "password": "cizZaORNokAdcbJFLEfhAGFfujozFYHk",  # substitua pela sua
-    "database": "railway"
-}
